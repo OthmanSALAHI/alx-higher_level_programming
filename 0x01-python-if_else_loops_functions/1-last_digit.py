e1c03e8abcbc122d06ec657c9e7a42f9692ceb2a
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-09-05 21:33:20 +0100

    pycodepython

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+digit = abs(number) % 10
 if number < 0:
-    number *= -1
-    last_digit = number % 10
-    last_digit *= -1
-    number *= -1
+    digit = -digit
+print(f"Last digit of {number:d} is {digit:d} and is ", end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
 else:
-    last_digit = number % 10
-    
-str = f"Last digit of {number:d}is {last_digit:d} "
-if last_digit > 5:
-    str+="and is greater than 5"
-elif last_digit == 0:
-    str+="and is 0"
-elif last_digit < 6 and last_digit != 0:
-    str+="and is less than 6 and not 0"
-print(str)
+    print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+1):
-    if chr(i) != 'q' and chr(i) != 'e':
-        print("{}".format(chr(i)), end="")
+for char in range(97, 123):
+    if chr(char) != 'q' and chr(char) != 'e':
+        print("{}".format(chr(char)), end="")
