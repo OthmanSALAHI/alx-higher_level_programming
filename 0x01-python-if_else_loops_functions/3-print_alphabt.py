3953d4f6069ba078c86f953f826ee38d02d29f8a
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-09-05 21:53:29 +0100

    no more time

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-    if chr(char) != 'q' and chr(char) != 'e':
-        print("{}".format(chr(char)), end="")
+# Author - Bamidele Adefolaju
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
