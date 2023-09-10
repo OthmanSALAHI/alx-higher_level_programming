e5db3ee6608ca704d89fa6084565f2c184c027aa
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-09-10 10:37:12 +0100

    first character

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if not sentence:
+        firstLetter = None
+    else:
+        firstLetter = sentence[0]
+    return length, firstLetter
+
+    
