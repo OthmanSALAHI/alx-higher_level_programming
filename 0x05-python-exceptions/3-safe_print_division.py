54038e2857627ffc23bd4f33efc0dea3e9134d33
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-09-25 13:02:26 +0100

    the value of division task commit num (#0)

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a / b
-    except (TypeError, ZeroDivisionError):
+    except (TypeError, ZeroDivisionError, ValueError):
         return None
     finally:
         print("Inside result:{}".format(result))
