9b0123be492b83540afb8ec2c7de8a7a1b015725
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-09-25 12:07:35 +0100

    init ignore

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    result = 0
+    for idx in range(x):
+        try:
+            print("{}".format(my_list[idx]), end="")
+            result += 1
+        except ValueError as e:
+            print("Error of {}".format(e))
+    print()
+    return (result)
