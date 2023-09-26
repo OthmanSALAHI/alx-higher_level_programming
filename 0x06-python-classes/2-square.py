ee91de4d78d551f346828331b88d1eed6e36073f
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-09-26 11:33:57 +0100

    the 2 square editing

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+"""the class"""
+
+
 class Square:
+    """the entry of class"""
+
     def __init__(self, size=0):
+        """init the class
+
+        Args:
+            size (int): the size of Square
+        """
         try:
             self.__size = size
         except TypeError:
             raise TypeError("size must be an integer")
         except ValueError:
             raise ValueError("size must be >= 0")
-
