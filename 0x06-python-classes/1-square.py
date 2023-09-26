bc4097528405f7902bdc273fef91d5ae1c77348c
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-09-26 11:28:43 +0100

    the last

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""the class pass"""
+
+
 class Square:
+    """the entry of class"""
     def __init__(self, size):
+        """initialise the init"""
+
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
 
