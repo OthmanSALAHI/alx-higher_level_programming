67d1ebab48221aa697635ed580deb01e70546343
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-10-02 10:26:53 +0100

    fixing the output incorrect

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -25,13 +25,13 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(self, val):
+    def width(self, value):
         """give value to width."""
-        if not isinstance(val, int):
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        elif val < 0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
-        self.__width = val
+        self.__width = value
 
     @property
     def height(self):
@@ -39,10 +39,10 @@ class Rectangle:
         return self.__height
 
     @width.setter
-    def height(self, val):
+    def height(self, value):
         """give value to height."""
-        if not isinstance(val, int):
+        if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        elif val < 0:
+        elif value < 0:
             raise ValueError('height must be >= 0')
-        self.__height = val
+        self.__height = value
