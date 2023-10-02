b851dc72b9f0bf6cd6b51600ed15871c48ab9704
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-10-02 10:25:28 +0100

    fixing the output incorrect

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,7 +3,17 @@
 
 
 class Rectangle:
-    """init the Rectangle."""
+    """init the Rectangle.
+    Args:
+        width (int): width
+        height (int): height
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
+    """
     def __init__(self, width=0, height=0):
         """the initial of rectangle."""
         self.width = width
