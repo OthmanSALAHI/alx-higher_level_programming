28759b33d6b3819c06b3ca813787f92bbb9fee6e
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-12-12 13:50:28 +0100

    the rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-    print() {
-      let i = 0;
-      while (i < this.height) {
-        let j = 0;
-        let strin = "";
-        while (j < this.width) {
-            strin += "X"
-            j++;
-        }
-        console.log(strin);
-        i++;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  module.exports = Rectangle;
-  
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      let j = 0;
+      let strin = '';
+      while (j < this.width) {
+        strin += 'X';
+        j++;
+      }
+      console.log(strin);
+      i++;
+    }
+  }
+}
+module.exports = Rectangle;
