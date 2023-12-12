2b3bfb7ab3457d778a26b24b602119cd5da19bb3
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-12-12 13:57:08 +0100

    the double and rotate

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      let j = 0;
+      let strin = '';
+      while (j < this.width) {
+        strin += 'X';
+        j++;
+      }
+      console.log(strin);
+      i++;
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
