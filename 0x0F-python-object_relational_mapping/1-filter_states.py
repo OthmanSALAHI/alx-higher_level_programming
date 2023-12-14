cb25bd1faba9091175578056e36451c9e6e08748
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-12-14 15:26:05 +0100

    states with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,7 @@ import MySQLdb
 
 if __name__ == '__main__':
     data_base = MySQLdb.connect(user=argv[1], passwd=argv[2],
-                               db=argv[3], port=3306)
+                                db=argv[3], port=3306)
     cur = data_base.cursor()
     cur.execute("SELECT * FROM states WHERE `name` LIKE 'N%';")
     states = cur.fetchall()
