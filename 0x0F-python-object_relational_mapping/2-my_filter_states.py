a6ba56ed01ea2cd9dca99ee076016ab0722fd457
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-12-14 21:36:32 +0100

    filter state

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from sys import argv
+import MySQLdb
+
+def connect_db():
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                                db=argv[3], port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE `name` = '{}'".format(argv[4]))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    db.close()
+
+if __name__ == "__main__":
+    connect_db()
