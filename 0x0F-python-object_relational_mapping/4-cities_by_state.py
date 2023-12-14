810189777fdaa01ca47ac78ab9185084105d9552
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-12-14 21:37:32 +0100

    the state with cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from sys import argv
+import MySQLdb
+
+def connect_sql():
+    db = MySQLdb.connect(user = argv[1], passwd = argv[2],
+                         db = argv[3], port = 3306)
+    cursor = db.cursor()
+    cursor.execute(f"SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id;")
+    result = cursor.fetchall()
+    for i in result:
+        print(i)
+    db.close()
+
+if __name__ == "__main__":
+    connect_sql()
