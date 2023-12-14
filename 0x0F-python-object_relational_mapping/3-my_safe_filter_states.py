73145e7454c42bdd4278de2dda78f8f96be98e78
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-12-14 21:36:59 +0100

    safe sql

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from sys import argv
+import MySQLdb
+
+def connect_sql():
+    db = MySQLdb.connect(user = argv[1], passwd = argv[2],
+                         db = argv[3], port = 3306)
+    name = argv[4]
+    cursor = db.cursor()
+    cursor.execute(f"SELECT * FROM states WHERE `name` = '{name}'")
+    result = cursor.fetchall()
+    for i in result:
+        print(i)
+    db.close()
+
+if __name__ == "__main__":
+    connect_sql()
