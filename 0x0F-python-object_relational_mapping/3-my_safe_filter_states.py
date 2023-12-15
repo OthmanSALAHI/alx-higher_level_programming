343c60e27ed0717bf2f88cbdb0f7345d33239449
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-12-15 13:33:29 +0100

    security matters

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
+"""the security matters in sql"""
+
 
 from sys import argv
 import MySQLdb
 
+
 def connect_sql():
     db = MySQLdb.connect(user=argv[1], passwd=argv[2],
-                                db=argv[3], port=3306)
-    name = argv[4]
+                         db=argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute(f"SELECT * FROM states WHERE `name` = '{name}'")
+    cursor.execute(f"SELECT * FROM states WHERE \
+    CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS like %s;", (argv[4],))
     result = cursor.fetchall()
     for i in result:
         print(i)
     db.close()
+
 
 if __name__ == "__main__":
     connect_sql()
