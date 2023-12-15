ae83f34e6afb3832512268b13768f0ee18f3e14e
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-12-15 18:30:36 +0100

    the id of state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""prints the first state"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3066/{}".
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    id_to_check = argv[4]
+    id_state = session.query(State).filter(State.id == id_to_check).first()
+    print(f"{id_state.name}" if id_state else "Not found")
