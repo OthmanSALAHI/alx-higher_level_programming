fc6aeb125cc39b5c7a4d1cea7c863e202ee66134
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-12-15 13:37:07 +0100

    the cities and the states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""the city and state"""
+
 
 from sys import argv
 import MySQLdb
 
+
 def connect_sql():
     db = MySQLdb.connect(user=argv[1], passwd=argv[2],
-                        db=argv[3], port=3306)
+                         db=argv[3], port=3306)
     cursor = db.cursor()
     cursor.execute(f"SELECT cities.id, cities.name, states.name \
     FROM cities JOIN states ON cities.state_id = states.id;")
@@ -13,6 +16,7 @@ def connect_sql():
     for i in result:
         print(i)
     db.close()
+
 
 if __name__ == "__main__":
     connect_sql()
