dbe9ae5a18834de281155904cbadd235f9318a3e
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-12-15 19:47:53 +0100

    the states and cities

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Contains State class and Base and City"""
+
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    sC = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+
+    for state in sC:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"    {city.id}: {city.name}")
