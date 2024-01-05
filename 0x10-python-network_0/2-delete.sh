68bd99a718a3d972394cdbc4c9104770d3c74409
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-01-05 20:02:27 +0100

    delete the HTTP requests

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Sends a GET request to a given URL and display the response status code.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# Send a DELETE request to a given URL and display the response body.
+curl -sX DELETE "$1"
 
